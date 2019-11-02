ae1329d3cbe65814410ab383f6b14416bc643152
Alyona <itsbronza@yahoo.com>
2019-11-02 19:12:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,25 +1,26 @@
 function callMe() {
+    var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
 
-  var lyric = "maybe";
+
     return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
+thisIsCrazy();
 
-
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
-    thisIsCrazy();
+    
 }
-crazy()
 
+  sayMy();
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
@@ -30,6 +31,6 @@ function sayMyName() {
     console.log(name);
     var name = "Kristin";
   }
-  sayMy();
+
 }
 sayMyName()
